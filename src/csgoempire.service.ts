caf286a138013f1bb57b211c144ee0100fe296dd
Gustavo <42685889+gustavo-dev@users.noreply.github.com>
2021-02-05 12:04:48 -0300

    Multiply percentage change by -1
    We multiply it by -1 to be able to compare it with the threshold set by the user (https://github.com/antal-k/csgoempire-deposit/pull/2#issuecomment-774079772)

--- src/csgoempire.service.ts
+++ src/csgoempire.service.ts
@@ -88,7 +88,7 @@ export class CsgoempireService {
                 const percent =
                     ((item.market_value - originalItemPrice) /
                         originalItemPrice) *
-                    100;
+                    100 * -1; // We multiply it by -1 to be able to compare it with the threshold set by the user
                 const prefix = percent > 0 ? '-' : '+';
                 this.helperService.sendMessage(`Price changed for ${item.market_name}, ${item.market_value / 100} => ${originalItemPrice / 100} - ${prefix}${(percent < 0 ? percent * -1 : percent)}%`, 'p2pItemUpdatedPriceChanged');
                 if (percent > config.delistThreshold) {
